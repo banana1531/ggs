07740830747f9a6bf37e9f42f17e4a042c07b73d
hwangmindong <64365043+hwangmindong@users.noreply.github.com>
2020-07-22 14:16:24 +0900

    정보오류 게시판 업데이트

---
+++ 99s/ggs/src/main/java/com/ggs/DTO/InfoErrorBoardDTO.java
@@ -0,0 +1,137 @@
+package com.ggs.DTO;
+
+import java.util.Date;
+
+public class InfoErrorBoardDTO {
+	
+	private int writeno;
+	private String id;
+	private String title;
+	private String content;
+	private Date writedate;
+	private int views;
+	private int isvisible;
+	private String imagesrc;
+	private String boardname;
+	
+	private int start;
+	private int end;
+	
+	private String searchType;
+	private String keyword;
+	
+	private int nowPage;
+
+	public int getWriteno() {
+		return writeno;
+	}
+
+	public void setWriteno(int writeno) {
+		this.writeno = writeno;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Date getWritedate() {
+		return writedate;
+	}
+
+	public void setWritedate(Date writedate) {
+		this.writedate = writedate;
+	}
+
+	public int getViews() {
+		return views;
+	}
+
+	public void setViews(int views) {
+		this.views = views;
+	}
+
+	public int getIsvisible() {
+		return isvisible;
+	}
+
+	public void setIsvisible(int isvisible) {
+		this.isvisible = isvisible;
+	}
+
+	public String getImagesrc() {
+		return imagesrc;
+	}
+
+	public void setImagesrc(String imagesrc) {
+		this.imagesrc = imagesrc;
+	}
+
+	public String getBoardname() {
+		return boardname;
+	}
+
+	public void setBoardname(String boardname) {
+		this.boardname = boardname;
+	}
+
+	public int getStart() {
+		return start;
+	}
+
+	public void setStart(int start) {
+		this.start = start;
+	}
+
+	public int getEnd() {
+		return end;
+	}
+
+	public void setEnd(int end) {
+		this.end = end;
+	}
+
+	public String getSearchType() {
+		return searchType;
+	}
+
+	public void setSearchType(String searchType) {
+		this.searchType = searchType;
+	}
+
+	public String getKeyword() {
+		return keyword;
+	}
+
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+
+	public int getNowPage() {
+		return nowPage;
+	}
+
+	public void setNowPage(int nowPage) {
+		this.nowPage = nowPage;
+	}
+	
+}
